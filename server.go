228ee59cb4ea4720609423fb077f3d26d5ee281a
Daniel Tsirlin <DanielTsirlin@Daniels-MacBook-Pro-2.local>
2018-12-19 16:29:45 +1100

    able to parse and manipulate params of route

--- server.go
+++ server.go
@@ -9,10 +9,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// The user Type
+type User struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+var users []User
+
 // Display a single data
 func ValidateUser(w http.ResponseWriter, r *http.Request) {
 
-	// params := mux.Vars(r)
 	// to do:
 	// search the userId param to see if it matches the id of any users
 	// if user not found, say invalid details (don't say user not found for better security)
@@ -20,20 +27,34 @@ func ValidateUser(w http.ResponseWriter, r *http.Request) {
 	// if yes, spit out jwt
 	// else say invalid details
 
-	// for _, item := range people {
-	// 	if item.ID == params["id"] {
-	// 		json.NewEncoder(w).Encode(item)
-	// 		return
-	// 	}
-	// }
-	// json.NewEncoder(w).Encode(&Person{})
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	params := mux.Vars(r)
+	foundUser := false
+	for _, user := range users {
+		if user.Username == params["userUsername"] {
+			// json.NewEncoder(w).Encode(item)
+			if user.Password == params["userPassword"] {
+				fmt.Fprintf(w, "Access granted to "+params["userUsername"]+":"+params["userPassword"])
+				// todo: generate and print jwt token to screen
+			} else {
+				fmt.Fprintf(w, "Invalid credentials")
+			}
+			return
+		}
+	}
+	if foundUser == false {
+		fmt.Fprintf(w, "Invalid credentials")
+	}
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	users = append(users, User{Username: "dantsirlin@gmail.com", Password: "testpass"})
+	users = append(users, User{Username: "seconduser@gmail.com", Password: "password"})
+	// todo: route all pages that aren't /validate/username/pass to root page which shows user how to correctly use the rest api
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/validate/{userId}/{userPassword}", ValidateUser).Methods("GET")
+	router.HandleFunc("/validate/{userUsername}/{userPassword}", ValidateUser).Methods("GET")
+	// todo: a route that takes in jwt token and loads screen showing welcome to that user/pass
+	// router.HandleFunc("/authjwt", ValidateJWT).Methods.("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
